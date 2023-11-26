89faa20dd2fafed0dc109c5cb93c0b6940b3ac9d
Nela <andzelamilowska@gmail.com>
2023-11-26 11:43:35 +0100

    xD

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+// import { Tooltip, Toast, Popover } from 'bootstrap';
 import './styles/main.scss'
+import { DateArrangements } from './app/date_arrangements';
+import { CalendarRendering } from './app/calendar_rendering';
+import { CalendarArrangements } from './app/calendar_arrangements';
+
+// DateArrangements.date.setMonth(1)
+CalendarRendering.runMonthRender(DateArrangements.month(), DateArrangements.year())
+CalendarArrangements.changeMonthListener()
